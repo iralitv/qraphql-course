2d01cd352deace383e028ee7d50cc3ae684268d3
Iryna Litvinchuk <ilitvinchuk@evoke.by>
2021-03-26 09:41:04 +0300

    feat: add frontend part to project

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BookingsPage extends Component {
+    render() {
+        return (
+            <h1>The bookings page</h1>
+        );
+    }
+}
+
+export default BookingsPage;
